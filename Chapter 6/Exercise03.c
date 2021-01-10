225af90da4ff278093e27550feb4beee2fbb0dd9
Cruzer-S <yyyynoom@gmail.com>
2021-01-11 00:27:43 +0900

    Chapter 6. Exercise 03

--- Chapter 6/Exercise03.c
+++ Chapter 6/Exercise03.c
@@ -1,36 +1,117 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-#define MAXWORDS 1024
+#define WORDSIZ 128
+#define NLINES 1
 
 struct list {
-	char word[MAXWORDS];
+	char *word;
 	int line_number;
+
 	struct list *next;
 };
 
+struct list *add_list(struct list *, char [], int );
+void print_list(struct list * );
+int getword(char [], int );
+
 int main(void)
 {
-	struct list *head = NULL;
+	struct list *head;
+	char word[WORDSIZ];
+	int type, line_number;
+
+	for (head = NULL, line_number = 0;
+		 (type = getword(word, WORDSIZ)) != EOF; )
+	{
+		if (type == NLINES) {
+			line_number++;
+			continue;
+		} else {
+			head = add_list(head, word, line_number);
+		}
+	}
+
+	print_list(head);
 
 	return 0;
 }
 
-struct list *add_data(struct list *ls, char word[], int line)
+int getword(char word[], int lim)
 {
-	if (ls == NULL) {
-		ls = lalloc();
+	int c;
+	char *w = word;
 
-		ls->next = NULL;
-		ls->line_number = line;
-		strcpy(ls->word, word);
-	} else {
-		while (ls != NULL && ls->line_number > line)
-			ls = ls->next;
+	while (isspace(c = getchar()))
+		if (c == '\n') return NLINES;
 
+	if (c != EOF)
+		*w++ = c;
+
+	if (!isalpha(c) || c == '_') {
+		*w = '\0';
+		return c;
 	}
+
+	for (; --lim > 0; w++)
+	{
+		if (!isalnum(*w = getchar()) && *w != '_') {
+			ungetc(*w, stdin);
+			break;
+		}
+	}
+
+	*w = '\0';
+
+	return word[0];
+}
+
+#include <string.h>
+
+struct list *lalloc(void);
+
+struct list *add_list(struct list *ls, char word[], int line)
+{
+	struct list *new_node = lalloc();
+	int comp;
+
+	if (ls == NULL) {
+		new_node->word = strdup(word);
+		new_node->line_number = line;
+		new_node->next = NULL;
+
+		ls = new_node;
+	} else {
+		struct list *cur, *prev;
+
+		for (prev = NULL, cur = ls;
+			 cur != NULL && (comp = strcmp(ls->word, word)) >= 0;
+			 prev = cur, cur = cur->next) ;
+
+		if (prev == NULL) {
+			if (comp == 0) {
+				for (;;);
+			} else {
+
+			}
+		} else {
+			new_node->next = cur;
+			prev->next = new_node;
+		}
+	}
+
+	return ls;
 }
 
 struct list *lalloc(void)
 {
 	return (struct list *) malloc(sizeof(struct list));
+}
+
+void print_list(struct list *ls)
+{
+	while (ls != NULL) {
+		printf("%s: %d", ls->word, ls->line_number);
+	}
 }

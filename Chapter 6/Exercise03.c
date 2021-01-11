8c42c7a96cbe11a396e45a0d8ab3c496b803a053
Cruzer-S <yyyynoom@gmail.com>
2021-01-12 05:41:03 +0900

    Chapter 6. Exercise03 Complete

--- Chapter 6/Exercise03.c
+++ Chapter 6/Exercise03.c
@@ -1,117 +1,122 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 
-#define WORDSIZ 128
-#define NLINES 1
+#define WRDLEN 128
+#define LINLEN 64 
 
 struct list {
 	char *word;
-	int line_number;
+	short line[LINLEN];
+	char usage;
 
 	struct list *next;
 };
 
-struct list *add_list(struct list *, char [], int );
-void print_list(struct list * );
-int getword(char [], int );
+int getword(char word[], int maxlen);
+
+struct list *add_list(struct list *, char *, int );
+void show_list(struct list *);
 
 int main(void)
 {
-	struct list *head;
-	char word[WORDSIZ];
-	int type, line_number;
+	struct list *head = NULL;
+	char word[WRDLEN];
+	int lines = 0, type;
 
-	for (head = NULL, line_number = 0;
-		 (type = getword(word, WORDSIZ)) != EOF; )
+	while ((type = getword(word, WRDLEN)) != EOF) 
 	{
-		if (type == NLINES) {
-			line_number++;
+		if (type == '\n') {
+			lines++;
 			continue;
-		} else {
-			head = add_list(head, word, line_number);
 		}
+
+		head = add_list(head, word, lines + 1);
 	}
 
-	print_list(head);
-
+	show_list(head);
+	
 	return 0;
 }
 
-int getword(char word[], int lim)
+#include <string.h>
+#include <stdlib.h>
+
+struct list *add_list(struct list *head, char *word, int line) 
+{
+	struct list *prev, *cur, *new_node;
+	int comp;
+	char *strdup(const char * );
+
+	new_node = malloc(sizeof(struct list));
+	if (new_node == NULL)
+		return NULL;
+
+	for (prev = NULL, cur = head;
+		 cur != NULL && (comp = strcmp(cur->word, word)) < 0;
+		 prev = cur, cur = cur->next) ;
+
+	if (cur == NULL) {
+		new_node->word = strdup(word);
+		new_node->line[0] = line;
+		new_node->usage = 1;	
+		new_node->next = NULL;
+
+		if (prev)
+			prev->next = new_node;
+		else
+			head = new_node;
+	} else {
+		if (comp == 0) {
+			if (cur->line[cur->usage - 1] != line)
+				cur->line[cur->usage++] = line;
+		} else {
+			new_node->word = strdup(word);
+			new_node->line[0] = line;
+			new_node->usage = 1;
+			new_node->next = cur;
+
+			if (prev)
+				prev->next = new_node;
+			else
+				head = new_node;
+		}
+	}
+
+	return head;
+}
+
+void show_list(struct list *ls) 
+{
+	int i;
+
+	for (; ls != NULL; ls = ls->next) {
+		printf("%s: %d", ls->word, ls->line[0]);
+
+		for (i = 1; i < ls->usage; i++)
+			printf(", %d", ls->line[i]);
+
+		printf("\n");
+	}
+}
+
+#include <ctype.h>
+
+int getword(char word[], int maxlen)
 {
 	int c;
 	char *w = word;
 
-	while (isspace(c = getchar()))
-		if (c == '\n') return NLINES;
-
-	if (c != EOF)
-		*w++ = c;
-
-	if (!isalpha(c) || c == '_') {
-		*w = '\0';
-		return c;
+	while (!isalpha(c = getchar())) {
+		if (c == EOF) return EOF;
+		if (c == '\n') return '\n';
 	}
 
-	for (; --lim > 0; w++)
-	{
-		if (!isalnum(*w = getchar()) && *w != '_') {
+	for (*w++ = c; --maxlen > 0; w++)
+		if (!isalpha(*w = getchar())) {
 			ungetc(*w, stdin);
 			break;
 		}
-	}
 
 	*w = '\0';
 
 	return word[0];
-}
-
-#include <string.h>
-
-struct list *lalloc(void);
-
-struct list *add_list(struct list *ls, char word[], int line)
-{
-	struct list *new_node = lalloc();
-	int comp;
-
-	if (ls == NULL) {
-		new_node->word = strdup(word);
-		new_node->line_number = line;
-		new_node->next = NULL;
-
-		ls = new_node;
-	} else {
-		struct list *cur, *prev;
-
-		for (prev = NULL, cur = ls;
-			 cur != NULL && (comp = strcmp(ls->word, word)) >= 0;
-			 prev = cur, cur = cur->next) ;
-
-		if (prev == NULL) {
-			if (comp == 0) {
-				for (;;);
-			} else {
-
-			}
-		} else {
-			new_node->next = cur;
-			prev->next = new_node;
-		}
-	}
-
-	return ls;
-}
-
-struct list *lalloc(void)
-{
-	return (struct list *) malloc(sizeof(struct list));
-}
-
-void print_list(struct list *ls)
-{
-	while (ls != NULL) {
-		printf("%s: %d", ls->word, ls->line_number);
-	}
 }
